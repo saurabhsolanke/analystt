3db0434d65d518fc2702842c16fdf834ed1580aa
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2023-02-02 08:30:03 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  const column = [
+    { heading: 'id', value: 'id'},
+    { heading: 'Name', value: 'name'},
+    { heading: 'Details', value: 'details'},
+    { heading: 'price', value: 'price'}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        header
       </header>
+      <ul>
+        {data.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
